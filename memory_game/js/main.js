6a0d82587174ab7413d1dd0915d4a9acab80a515
viviRbi <50475727+viviRbi@users.noreply.github.com>
2019-10-05 14:28:35 -0500

    create game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+alert("running")
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
