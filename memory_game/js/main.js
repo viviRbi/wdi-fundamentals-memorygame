b194aaf519976f9ba54290169aab1aa734838197
viviRbi <50475727+viviRbi@users.noreply.github.com>
2019-10-06 16:48:51 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
-alert("running")
-console.log("Up and running!");
+cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+cardsInPlay=[];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+
+console.log("use flipped queen");
+
+var cardTwo = cards[1];
+cardsInPlay.push("cardOne");
+
+console.log("use flipped king");
+
+if (cardsInPlay.length === 2) {
+	console.log("king a");
+	if (cardsInPlay[0] === cardsInPlay[4] ) {
+		alert("You found a match.");
+	}
+	else {
+		alert("Sorry. Try again!");
+	}
+}else {
+	alert("something");
+}
